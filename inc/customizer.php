04b4be62c74d97aa4d972ae56d9bc7285c9763f0
Alexey Leontiev <web-a.leon@ukr.net>
2016-12-07 02:13:49 +0200

    basic underscores template and TGM plugin activator

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * englishdeutsch Theme Customizer.
+ *
+ * @package englishdeutsch
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function englishdeutsch_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'englishdeutsch_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function englishdeutsch_customize_preview_js() {
+	wp_enqueue_script( 'englishdeutsch_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'englishdeutsch_customize_preview_js' );
